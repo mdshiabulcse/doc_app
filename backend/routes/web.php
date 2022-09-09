618838b565cd8a0aa5db7b28451175c42cd4e035
md.shiabul.cse@gmail.com <md.shiabul.cse@gmail.com>
2022-09-09 19:30:46 +0600

    first commit

---
+++ backend/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ShopController;
+use App\Models\ShopModel;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $datas= ShopModel::where('status', 1)->get();
+    return view('welcome', compact('datas'));
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/shop', ShopController::class);
+
